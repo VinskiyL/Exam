2b9170eabd1ca0eb28b0aed26e9e5d0d18e2f81b
VinskiyL <katuhatm@gmail.com>
2025-01-20 03:14:36 +0300

    Добавьте файлы проекта.

---
+++ Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruskal
+{
+    internal class Graph
+    {
+        public int[,] adj;
+        public int num;
+        public List<int>[] adj_list;
+        public Graph(int num)
+        {
+            this.num = num;
+            adj = new int[num, num];
+            adj_list = new List<int>[num];
+            for (int i = 0; i < num; i++) { adj_list[i] = new List<int>(); }
+        }
+        public void Add(int start, int end, int weight)
+        {
+            adj[start, end] = weight;
+            adj[end, start] = weight;
+            adj_list[start] = new List<int>(end);
+        }
+    }
+}
---
+++ OstTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruskal
+{
+    internal class OstTree
+    {
+        Graph graph;
+        bool[] bools;
+
+        public OstTree(Graph graph)
+        {
+            this.graph = graph;
+            bools = new bool[graph.num];
+            for (int i = 0; i < graph.num; i++)
+            {
+                bools[i] = true;
+            }
+        }
+        void FindMin()
+        {
+            int min = 100;
+            for (int i = 0; i < graph.num; i++)
+            {
+                if (bools[i] && graph.adj[i, j] < min) 
+            }
+        }
+        void FindElem(int[,] array, int i, int j, int k, bool[] flag)
+        {
+            for (int v = j; v < k; v++)
+            {
+                if (array[i, v] == 1 && flag[v])
+                {
+                    Console.Write(v + " ");
+                    flag[v] = false;
+                    FindElem(array, v, ++j, k, flag);
+                }
+            }
+
+        }
+        public void DFS(Graph g)
+        {
+            bool[] flag = new bool[g.numEdges];
+            for (int i = 0; i < g.numEdges; i++) { flag[i] = true; }
+            Console.WriteLine("Вершины в порядке обхода в глубину");
+            Console.Write(0 + " ");
+            FindElem(g.adjacency, 0, 0, g.numEdges, flag);
+        }
+    }
+}
